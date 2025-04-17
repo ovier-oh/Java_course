ddfb5e56e3c263626c7c47c36962abdbbaf73a25
ovier-oh <73931153+ovier-oh@users.noreply.github.com>
2025-04-17 12:59:09 +0000

    add this project about task manager

---
+++ manager_task.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class gestor_tarea {
+    public static void main(String[] args) {
+        System.out.println("*** Task manager ***");
+        System.out.println("> Choice one option.");
+        System.out.println("[1] Add Task. \n[2] Show Task \n[3] Edit task \n[4] Delete task");
+        System.out.println("[5] Mark task as completed \n[6] Organize task \n[7] Exit");
+        Scanner input = new Scanner(System.in);
+        String select = input.nextLine();
+
+        switch(select){
+            case "1":
+                break;
+            case "2":
+                break;
+                
+        }
+
+    }
+}
