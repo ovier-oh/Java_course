fc44bb953d04ebf9de7b77a15f513b8722954653
ovier-oh <ovier.obregon.hin@outlook.com>
2025-04-18 22:16:45 +0300

    empezando el proyecto

---
+++ to_do.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class to_do {
+    public static void task_list (){
+        ArrayList<Integer> list0 = new ArrayList<>();
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("*** To Do list ***");
+        System.out.println("[1] Add Task");
+        System.out.println("[2] Show Tasks");
+        System.out.println("[3] Delete Tasks");
+        System.out.println("[4] Exit.");
+    }
+}
