eb7073386bf4627c93452f110b174f5ca4a7eff4
ovier-oh <73931153+ovier-oh@users.noreply.github.com>
2025-05-13 15:15:13 +0000

    Add greetings

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,13 @@
 public class TicTacToe{
     public static int matrixtoe[][] = {{1,2,3},{4,5,6},{7,8,9}}; 
 
+    public static void greetings(){
+        System.out.println("**Welcome to TicTacToe Game**");
+        System.out.println("[Info] The First participant in create 3 in line Wins!!");
+        System.out.println("[Info] You can choose one player [O] or [X]");
+        System.out.println("[Info] Please look the game board, here one number is one position");
+        System.out.println("[Info] You only need write the number of position that you want :D");
+    }
     public static void matix_tictactoe(){
         System.out.println(matrixtoe[0][0] + " | " + matrixtoe[0][1] + " | "+ matrixtoe[0][2]);
         System.out.println("-- " + "-- " + "--");
@@ -10,6 +17,7 @@ public class TicTacToe{
     }
 
     public static void main(String[] args){
+        greetings();
         matix_tictactoe();
         
     }
